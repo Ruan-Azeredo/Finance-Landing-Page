07a44018c9e5e9261d11d98f2bd1b5505e0568f6
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-26 18:41:07 -0300

    :goal_net: fix: update data footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,30 +34,30 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <h6 className="font-medium text-gray-400">Solution</h6>
+                    <h6 className="font-medium text-gray-400">Support</h6>
                     <ul>
-                        <li className="py-2 text-sm">Analytics</li>
-                        <li className="py-2 text-sm">Marketing</li>
-                        <li className="py-2 text-sm">Commerce</li>
-                        <li className="py-2 text-sm">Insights</li>
+                        <li className='py-2 text-sm'>Pricing</li>
+                        <li className='py-2 text-sm'>Documentation</li>
+                        <li className='py-2 text-sm'>Guides</li>
+                        <li className='py-2 text-sm'>API Status</li>
                     </ul>
                 </div>
                 <div>
-                    <h6 className="font-medium text-gray-400">Solution</h6>
+                    <h6 className="font-medium text-gray-400">Company</h6>
                     <ul>
-                        <li className="py-2 text-sm">Analytics</li>
-                        <li className="py-2 text-sm">Marketing</li>
-                        <li className="py-2 text-sm">Commerce</li>
-                        <li className="py-2 text-sm">Insights</li>
+                        <li className='py-2 text-sm'>About</li>
+                        <li className='py-2 text-sm'>Blog</li>
+                        <li className='py-2 text-sm'>Jobs</li>
+                        <li className='py-2 text-sm'>Press</li>
+                        <li className='py-2 text-sm'>Careers</li>
                     </ul>
                 </div>
                 <div>
-                    <h6 className="font-medium text-gray-400">Solution</h6>
+                    <h6 className="font-medium text-gray-400">Legal</h6>
                     <ul>
-                        <li className="py-2 text-sm">Analytics</li>
-                        <li className="py-2 text-sm">Marketing</li>
-                        <li className="py-2 text-sm">Commerce</li>
-                        <li className="py-2 text-sm">Insights</li>
+                        <li className='py-2 text-sm'>Claim</li>
+                        <li className='py-2 text-sm'>Policy</li>
+                        <li className='py-2 text-sm'>Terms</li>
                     </ul>
                 </div>
             </div>
