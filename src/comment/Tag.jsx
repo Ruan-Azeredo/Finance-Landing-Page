9094ebb30e233a00f033044e401f82ba9118d537
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-26 14:44:05 -0300

    :iphone: fix: responsiva tag

--- src/comment/Tag.jsx
+++ src/comment/Tag.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Tag = () => {
     return (
-        <div className='fixed top-[85vh] right-5 p-3 bg-blue-300 tag'>
+        <div className='fixed top-[85vh] right-5 p-3 bg-blue-300 tag md:w-fit w-[375px]'>
             <div className='mb-2 font-medium mx-auto w-fit'>Projeto desenvolvido para Portifolio.</div>
             <div>Inspecione a página para visualizar a <span className='font-medium'>Responsividade</span>.</div>
             <style>{`
