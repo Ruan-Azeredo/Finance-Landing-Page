ed8847094123a8c600e96e893fba1ffe22889827
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-26 13:12:03 -0300

    :construction: build: adiciona Cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
-      <Newsletter/>
+      <Newsletter />
+      <Cards/>
     </div>
   );
 }
